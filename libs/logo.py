dec9b7b7fc163c1a8f34abbb6858fd509e17ce3b
ll-mahto-hacker-ll <mahtohacker15@gmail.com>
2026-02-26 21:48:58 +0530

    [ꝛᴧϻ ηєᴛᴡσᴙᴋ]

--- libs/logo.py
+++ libs/logo.py
@@ -1,1 +1,27 @@
-.
+# coding=utf-8
+#!/usr/bin/env python3
+
+from libs.animation import colorText
+
+logo = '''
+
+[[black-bright-background]][[red]] ██▓ [[green]]███▄    █  [[blue]] ██████ [[magenta]]▄▄▄█████▓ [[cyan]]▄▄▄         [[red]] ██▀███  [[green]]▓█████ [[blue]] ██▓███   [[magenta]]▒█████   [[cyan]]██▀███  [[yellow]]▄▄▄█████▓[[reset]]
+[[black-bright-background]][[red]]▓██▒ [[green]]██ ▀█   █ ▒[[blue]]██    ▒ [[magenta]]▓  ██▒ ▓▒▒[[cyan]]████▄       [[red]]▓██ ▒ ██▒[[green]]▓█   ▀ [[blue]]▓██░  ██▒▒[[magenta]]██▒  ██▒▓[[cyan]]██ ▒ ██▒[[yellow]]▓  ██▒ ▓▒[[reset]]
+[[black-bright-background]][[red]]▒██▒▓[[green]]██  ▀█ ██▒░[[blue]] ▓██▄   [[magenta]]▒ ▓██░ ▒░▒[[cyan]]██  ▀█▄     [[red]]▓██ ░▄█ ▒[[green]]▒███   [[blue]]▓██░ ██▓▒▒[[magenta]]██░  ██▒▓[[cyan]]██ ░▄█ ▒[[yellow]]▒ ▓██░ ▒░[[reset]]
+[[black-bright-background]][[red]]░██░▓[[green]]██▒  ▐▌██▒ [[blue]] ▒   ██▒[[magenta]]░ ▓██▓ ░ ░[[cyan]]██▄▄▄▄██    [[red]]▒██▀▀█▄  [[green]]▒▓█  ▄ [[blue]]▒██▄█▓▒ ▒▒[[magenta]]██   ██░▒[[cyan]]██▀▀█▄  [[yellow]]░ ▓██▓ ░ [[reset]]
+[[black-bright-background]][[red]]░██░▒[[green]]██░   ▓██░▒[[blue]]██████▒▒[[magenta]]  ▒██▒ ░  [[cyan]]▓█   ▓██▒   [[red]]░██▓ ▒██▒[[green]]░▒████▒[[blue]]▒██▒ ░  ░░[[magenta]] ████▓▒░░[[cyan]]██▓ ▒██▒[[yellow]]  ▒██▒ ░ [[reset]]
+[[black-bright-background]][[red]]░▓  ░[[green]] ▒░   ▒ ▒ ▒[[blue]] ▒▓▒ ▒ ░[[magenta]]  ▒ ░░    [[cyan]]▒▒   ▓▒█░   [[red]]░ ▒▓ ░▒▓░[[green]]░░ ▒░ ░[[blue]]▒▓▒░ ░  ░░[[magenta]] ▒░▒░▒░ ░[[cyan]] ▒▓ ░▒▓░[[yellow]]  ▒ ░░   [[reset]]
+[[black-bright-background]][[red]] ▒ ░░[[green]] ░░   ░ ▒░░[[blue]] ░▒  ░ ░[[magenta]]    ░     [[cyan]] ▒   ▒▒ ░   [[red]]  ░▒ ░ ▒░[[green]] ░ ░  ░[[blue]]░▒ ░      [[magenta]] ░ ▒ ▒░  [[cyan]] ░▒ ░ ▒░[[yellow]]    ░    [[reset]]
+[[black-bright-background]][[red]] ▒ ░ [[green]]  ░   ░ ░ ░[[blue]]  ░  ░  [[magenta]]  ░       [[cyan]] ░   ▒      [[red]]  ░░   ░ [[green]]   ░   [[blue]]░░       ░[[magenta]] ░ ░ ▒   [[cyan]] ░░   ░ [[yellow]]  ░      [[reset]]
+[[black-bright-background]][[red]] ░   [[green]]        ░  [[blue]]     ░  [[magenta]]          [[cyan]]     ░  ░   [[red]]   ░     [[green]]   ░  ░[[blue]]          [[magenta]]   ░ ░   [[cyan]]  ░     [[yellow]]         [[reset]]
+                                                                                                  
+
+                                           [[black-bright-background]][[white]]Codded By Crevil[[reset]]
+                                            [[black]]Version :- 2.01[[reset]]
+
+                                           
+                                       [[red]]Telegram :- [[blue]]@Zhangweii[[reset]]
+'''
+
+def print_logo():
+    print(colorText(logo))
